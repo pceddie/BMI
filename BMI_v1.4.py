6695f3fd9c95cfce8806795b296a0413579bc8e5
Eddie Chen <pceddie@gmail.com>
2022-01-19 23:49:56 +0800

    BMI calculator v1.4

--- BMI_v1.4.py
+++ BMI_v1.4.py
@@ -1,4 +1,14 @@
 high = input('請問您的身高是多少公尺?')
 weight = input('請問您的體重是多少公斤?')
 result = float(weight)//float(high) ** 2
+import os
+
+def clearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+clearConsole()
+
 print('您的BMI值: ', result)
